1f72c491fb3da90eeb32cc0a583d59fc0d1e4922
sivaram000 <katta.sivaram@suprsend.com>
2023-02-04 12:47:03 +0530

    added inbox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SuprSendInboxService } from '@suprsend/ngx-inbox';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-example';
+  toasterService;
+
+  constructor(private ssinbox: SuprSendInboxService, toastr: ToastrService) {
+    this.toasterService = toastr;
+  }
+
+  ngOnInit() {
+    this.ssinbox.identifyUser('your distinct id', 'your subscriber id');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SuprSendInboxModule } from '@suprsend/ngx-inbox';
+import { ToastNoAnimationModule } from 'ngx-toastr';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ToastNoAnimationModule.forRoot(),
+    SuprSendInboxModule.forRoot({
+      workspaceKey: 'your workspace key',
+      workspaceSecret: 'your workspace secret',
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
